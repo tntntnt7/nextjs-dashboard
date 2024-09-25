7f9830cb0b30dfa8c03115a2383f455d4e83a57e
tntntnt7 <tntntnt7@gmail.com>
2024-09-25 18:56:11 +0800

    step 16

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  // title: 'Acme Dashboard', // 固定title
+  title: {
+    template: '%s | Acme Dashboard', // 每个page单独的title, 参见 /app/dashboard/invoices/page.tsx 中的title设置
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
